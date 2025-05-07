687469ea7392a549fcd79dd357e2b8e838b7c751
HibernalGlow <154863885+HibernalGlow@users.noreply.github.com>
2025-05-07 10:27:11 +0800

    feat: 添加新的config.json文件，包含布局和预设配置

--- picsconvert/__main__.py
+++ picsconvert/__main__.py
@@ -10,12 +10,12 @@ from functools import partial # 新增导入
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_dir)
-from picsconvert.utils.input_handler import InputHandler
-from picsconvert.convert.format_convert import ArchiveConverter,SUPPORTED_ARCHIVE_FORMATS
-from picsconvert.convert.performance_control import get_performance_params,start_config_gui_thread
+from .utils.input_handler import InputHandler
+from .convert.format_convert import ArchiveConverter,SUPPORTED_ARCHIVE_FORMATS
+from .convert.performance_control import get_performance_params,start_config_gui_thread
 # 导入黑名单文件路径
-from picsconvert.convert.compression_tracker import BLACKLIST_FILE_PATH
-from picsconvert.utils.monitor_decorator import infinite_monitor
+from .convert.compression_tracker import BLACKLIST_FILE_PATH
+from .utils.monitor_decorator import infinite_monitor
 from textual_preset import create_config_app
 from textual_logger import TextualLoggerManager
 import zipfile
